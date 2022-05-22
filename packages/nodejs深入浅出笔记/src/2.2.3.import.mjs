ee0b6ec9c86d035ba77bb65c8bb2c23c8aafc8c3
xiaoqinvar <17683860320m0@sina.cn>
2022-05-22 21:49:13 +0800

    ejs和esm的细节

--- packages/nodejs深入浅出笔记/src/2.2.3.import.mjs
+++ packages/nodejs深入浅出笔记/src/2.2.3.import.mjs
@@ -4,5 +4,9 @@ import export1 from './2.2.3.export.mjs'; // 存在import提升
 import export2 from './2.2.3.export.mjs';
 console.log(2);
 console.log(export1 === export2);
-export1.name = 'abc';
+export1.name = 'abc'; // 修改的是缓存的，再次引入的都会从缓存加载，所以结果一致
 console.log(export1, export2);
+// 异步加载
+import('./2.2.3.export.mjs').then((res) => {
+  console.log(res.default);
+})
