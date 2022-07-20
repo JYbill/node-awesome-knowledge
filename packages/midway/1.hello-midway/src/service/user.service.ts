8ad2626ad8809d9f3ab324d0ca6f9786520d6798
xiaoqinvar <17683860320m0@sina.cn>
2022-07-20 14:07:17 +0800

    获取依赖注入容器

--- packages/midway/1.hello-midway/src/service/user.service.ts
+++ packages/midway/1.hello-midway/src/service/user.service.ts
@@ -1,8 +1,19 @@
-import { Provide } from '@midwayjs/decorator';
+import { Context } from '@midwayjs/koa';
+import { Inject, Provide, Scope, ScopeEnum } from '@midwayjs/decorator';
 import { IUserOptions } from '../interface';
 
 @Provide()
+@Scope(ScopeEnum.Singleton)
 export class UserService {
+  count: number = 0;
+
+  constructor() {
+    console.log('init.');
+  }
+
+  @Inject()
+  ctx: Context;
+
   async getUser(options: IUserOptions) {
     return {
       uid: options.uid,
@@ -10,5 +21,10 @@ export class UserService {
       phone: '12345678901',
       email: 'xxx.xxx@xxx.com',
     };
+  }
+
+  async getCount() {
+    console.log(this.ctx);
+    return this.count++;
   }
 }
