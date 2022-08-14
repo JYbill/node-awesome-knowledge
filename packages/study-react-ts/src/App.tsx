5d75571b490dae93b324d9a950108558ae4a6ac0
xiaoqinvar <17683860320m0@sina.cn>
2022-08-14 15:40:20 +0800

    react study to init.

---
+++ packages/study-react-ts/src/App.tsx
@@ -0,0 +1,12 @@
+import Child1 from "./ref-dom/Child1";
+
+export default function App() {
+  return (
+    <div className="App">
+      <p>hello world</p>
+      <hr />
+
+      <Child1 />
+    </div>
+  );
+}
---
+++ packages/study-react-ts/vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from "vite";
+import type { UserConfig } from "vite"; 
+import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
+import tsconfigPaths from "vite-tsconfig-paths";
+import mkcert from "vite-plugin-mkcert";
+
+export default defineConfig(({ command, mode }) => {
+  const config: UserConfig = {
+    server: {
+      https: true,
+    },
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      legacy(),
+      mkcert({
+        source: "coding",
+      }),
+    ],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: ["react"],
+            "react-dom": ["react-dom"],
+          },
+        },
+      },
+    },
+  };
+  return config;
+});
