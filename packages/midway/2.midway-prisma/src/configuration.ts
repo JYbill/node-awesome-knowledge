aff8aeb0e7111d1b456589bb5b74fd7b8c5f44c3
xiaoqinvar <17683860320m0@sina.cn>
2022-09-23 21:51:51 +0800

    ok

--- packages/midway/2.midway-prisma/src/configuration.ts
+++ packages/midway/2.midway-prisma/src/configuration.ts
@@ -42,6 +42,6 @@ export class ContainerLifeCycle implements ILifeCycle {
     // 关闭prisma 连接
     const prismaClient = this.prismaClientServiceFactory.get();
     await prismaClient.$disconnect();
-    // test verify
+    // test verify1
   }
 }
