7ce2d068ad7fc85b7a7b908fe8e40bd8b8e70729
xiaoqinvar <17683860320m0@sina.cn>
2022-10-20 11:52:57 +0800

    learn: ts this类型工具

--- packages/typescript/10-this的内置工具.ts
+++ packages/typescript/10-this的内置工具.ts
@@ -12,6 +12,35 @@ type GetThisType<T> = T extends (this: infer R, ...args: any[]) => any ? R : "";
 type FooThisType = GetThisType<FooType>; */
 
 // 获取this内置工具
-type FooThisType = ThisParameterType<typeof foo>;
+// type FooThisType = ThisParameterType<typeof foo>;
+
+// 使用OmitThisParameter工具类型
+// type FooType = OmitThisParameter<typeof foo>;
+
+// 使用ThisType工具
+interface IPerson {
+  name: string;
+  age: number;
+}
+interface IState {
+  man: IPerson;
+  run(): void;
+  say(): void;
+}
+const state: IState | ThisType<IPerson> = {
+  man: {
+    name: "xiaoqinvar",
+    age: 23,
+  },
+  run() {
+    // this: IPerson
+    console.log(this.name);
+  },
+  say() {
+    // this: IPerson
+    console.log(this.name);
+  },
+};
+(state as IState).run.call((state as IState).man);
 
 export {};
---
+++ packages/typescript/11-面向对象.ts
@@ -0,0 +1,6 @@
+/**
+ * @file: 11-面向对象.ts
+ * @author: xiaoqinvar
+ * @desc：面向对象
+ * @date: 2022-10-20 11:52:47
+ */
