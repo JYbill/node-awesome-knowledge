ea2905517f3562cfa5aab8ff4ddc226e189607af
xiaoqinvar <17683860320m0@sina.cn>
2022-10-29 11:46:43 +0800

    learn: 封装ts, init webpack combine axios with ts encapsulation

---
+++ packages/typescript/04-axios封装/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "./src"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".mjs", ".cjs", ".json", ".ts"],
+  },
+  devServer: {},
+  module: {
+    rules: [
+      {
+        test: /.ts$/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
