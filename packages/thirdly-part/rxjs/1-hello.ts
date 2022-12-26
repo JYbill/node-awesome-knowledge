b3f4dfef7212533101186e3682fdb53fde871ca7
xiaoqinvar <17683860320m0@sina.cn>
2022-12-26 15:55:55 +0800

    init: 初始化midway-redis

--- packages/thirdly-part/rxjs/1-hello.ts
+++ packages/thirdly-part/rxjs/1-hello.ts
@@ -1,4 +1,27 @@
-import { interval } from "rxjs";
-import { take } from "rxjs/operators";
+import {first, interval, map, of} from "rxjs";
+import {take} from "rxjs/operators";
 
 interval(500).pipe(take(10)).subscribe(console.log);
+
+// 1. map操作符
+/*of(1, 2, 3)
+    .pipe(
+        map((data) => {
+            return data ** 2;
+        })
+    )
+    .subscribe((data) => {
+        console.log(data);
+    })*/
+/*
+* result:
+*   1 4 9
+* */
+
+// 2. first
+/*of(1, 2, 3)
+    .pipe(first())
+    .subscribe((data) => {
+        console.log("res", data);
+    })*/
+// res 1
