e01b31d34d2ae3c223a553d2d8261081be46d71e
xiaoqinvar <17683860320m0@sina.cn>
2023-02-05 10:14:27 +0800

    learn: 击鼓传花面试题

---
+++ packages/数据结构与算法/06.面试题:击鼓传花.ts
@@ -0,0 +1,31 @@
+import ArrayQueue from "./05.ArrayQueue实现";
+
+/**
+ * @Description: 面试题：击鼓传花（队列解决）
+ * @Author: 小钦var
+ * @Date: 2023/2/5 10:03
+ */
+function hotPotato(nameList: string[], num: number): number {
+  const arrayQueue = new ArrayQueue<string>();
+  // 插入队列
+  for (const name of nameList) {
+    arrayQueue.enQueue(name);
+  }
+  // 开始击鼓传花，过滤到所有成员只有一个时，结束击鼓传花
+  while (arrayQueue.size() > 1) {
+    for (let i = 1; i < num; i++) {
+      const name: string = arrayQueue.delQueue() as string;
+      arrayQueue.enQueue(name);
+    }
+    arrayQueue.delQueue();
+  }
+
+  // 花获谁家
+  const latestName = arrayQueue.delQueue() as string;
+  console.log("花落: ", latestName);
+  return nameList.indexOf(latestName);
+}
+
+const nameList = ["小张", "小明", "小青蛙", "小钦", "xqv", "xiaoqinvar"];
+const index = hotPotato(nameList, 1);
+console.log("中奖索引: ", index);
