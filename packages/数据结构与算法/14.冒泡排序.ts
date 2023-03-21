9faa8457c63fdd1dc787156406d786485a41044a
xiaoqinvar <17683860320m0@sina.cn>
2023-03-21 20:39:51 +0800

    learn: 冒泡排序🫧

---
+++ packages/数据结构与算法/14.冒泡排序.ts
@@ -0,0 +1,35 @@
+/**
+ * @Description: 冒泡排序
+ * @Author: 小钦var
+ * @Date: 2023/3/21 19:46
+ */
+
+/**
+ * 假设一个数组有5个元素进行排序
+ * 第一层循环：需要冒泡的次数，也就是 i < length - 1 = 5 - 1 -> [0, 3] 只需要4次
+ * 第二层循环：保证数组不越界 length - 1，且之前的排序会冒泡到最后一个元素，j < length - 1 - i
+ * @param list
+ */
+function bubblingSort(list: number[]): number[] {
+  for (let i = 0; i < list.length - 1; i++) {
+    for (let j = 0; j < list.length - 1 - i; j++) {
+      if (list[j] > list[j + 1]) {
+        list[j] = list[j] + list[j + 1];
+        list[j + 1] = list[j] - list[j + 1];
+        list[j] = list[j] - list[j + 1];
+      }
+    }
+  }
+  return list;
+}
+
+function main() {
+  const list = new Array(20);
+  for (let i = 0; i < 20; i++) {
+    list[i] = Math.ceil(Math.random() * 100);
+  }
+  console.log("排序前：", list.join(", "));
+  const sortedList = bubblingSort(list);
+  console.log("排序后：", sortedList.join(", "));
+}
+main();
