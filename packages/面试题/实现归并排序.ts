6f8622ef2cee6123f90cfe0db1383a622eca6ea6
xiaoqinvar <17683860320m0@sina.cn>
2023-06-05 14:08:30 +0800

    feat：6.5巩固排序算法、学习threejs

--- packages/面试题/实现归并排序.ts
+++ packages/面试题/实现归并排序.ts
@@ -1,5 +1,5 @@
 /**
- * @Description: 打卡5.29✅
+ * @Description: 打卡06.05✅
  * @Author: 小钦var
  * @Date: 2023/3/30 19:16
  */
---
+++ packages/面试题/实现防抖函数.ts
@@ -0,0 +1,28 @@
+type AnyFuncType = (...args: any[]) => any;
+
+export function debounce<This = any>(callback: AnyFuncType, timeout: number) {
+  let timer: NodeJS.Timer | null = null;
+
+  return function (this: This, ...args: any[]) {
+    if (timer) return;
+    callback.apply(this, args);
+    timer = setTimeout(() => {
+      clearTimeout(timer as NodeJS.Timer);
+      timer = null;
+    }, timeout);
+  };
+}
+
+function main() {
+  let i = 0;
+  const func = () => console.log(`running... ${i}`);
+  const runFunc = debounce<any>(func, 1000);
+
+  const timer = setInterval(() => {
+    if (i >= 10) clearInterval(timer);
+    runFunc();
+    i++;
+  }, 500);
+}
+
+main();
