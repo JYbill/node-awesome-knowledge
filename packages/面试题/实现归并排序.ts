11c7e8b792b10b2750263fb329700a5f75e5f0f5
xiaoqinvar <17683860320m0@sina.cn>
2023-08-01 10:18:54 +0800

    learn：复习排序算法

--- packages/面试题/实现归并排序.ts
+++ packages/面试题/实现归并排序.ts
@@ -1,5 +1,5 @@
 /**
- * @Description: 打卡07.24✅
+ * @Description: 打卡08.1✅
  * @Author: 小钦var
  * @Date: 2023/3/30 19:16
  */
--- packages/面试题/实现快速排序.ts
+++ packages/面试题/实现快速排序.ts
@@ -1,5 +1,5 @@
 /**
- * @Description: 打卡07.24✅
+ * @Description: 打卡8.1✅
  * @Author: 小钦var
  * @Date: 2023/3/30 20:14
  */
