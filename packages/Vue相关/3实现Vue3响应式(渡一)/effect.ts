8abcfd7b31fec3a2f97ab287bde90e681610dd6c
xiaoqinvar <17683860320m0@sina.cn>
2024-03-18 11:25:57 +0800

    feat(Vue3源码): 依赖收集数据解构完成

--- packages/Vue相关/3实现Vue3响应式(渡一)/effect.ts
+++ packages/Vue相关/3实现Vue3响应式(渡一)/effect.ts
@@ -1,8 +1,8 @@
 import { Read, Write } from "./operation";
 
-type activeEffectType = null | ((...args: any[]) => any);
-let activeEffect: activeEffectType = null;
-const targetMap = new WeakMap();
+type ActiveEffectType = null | ((...args: any[]) => any);
+let activeEffect: ActiveEffectType = null;
+const targetMap = new WeakMap<WeakKey, Map<string | symbol, any>>();
 export function effect(fn: (...args: any[]) => any) {
   const effectWrapper = () => {
     activeEffect = fn;
@@ -27,25 +27,36 @@ export function resumeTrigger() {
   triggerStatus = true;
 }
 
+const ITERATOR_KEY = Symbol("iterator");
 export function trace(target: any, operation: Read, key: string) {
   if (!triggerStatus || !activeEffect) return; // 依赖收集状态为暂停 || 用户的副作用没有执行，都停止依赖收集直接返回
 
-  if (operation === Read.ITERATE) {
-    // ⚠️ ITERATE一般是循环，是没有key的
-    console.log("trace", target);
-    return;
+  let propsMap = targetMap.get(target); // 属性Map
+  if (!propsMap) {
+    const map = new Map();
+    targetMap.set(target, map);
+    propsMap = map;
   }
-  console.log(
-    "trace",
-    "target",
-    // target,
-    // "opera = ",
-    operation,
-    "key = ",
-    key,
-    ", value =",
-    target[key]
-  );
+  let typeKey: undefined | string | symbol;
+  if (operation === Read.ITERATE) {
+    typeKey = ITERATOR_KEY; // ⚠️ ITERATE一般是循环，是没有key的
+  } else {
+    typeKey = key;
+  }
+  let typeMap: undefined | Map<string | symbol, Set<any>> = propsMap.get(typeKey); // 类型Map
+  if (!typeMap) {
+    const map = new Map();
+    propsMap.set(target, map)
+    typeMap = map;
+  }
+  let deps = typeMap.get(operation); // 依赖收集的Set，内部全是effect函数
+  if (!deps) {
+    const set = new Set<ActiveEffectType>([activeEffect]);
+    typeMap.set(operation, set);
+    deps = set;
+  }
+
+  console.log("debug", propsMap);
 }
 
 export function trigger(
