bf2905de9a95975bf3b0e460d63d9e46c18a3338
xiaoqinvar <17683860320m0@sina.cn>
2024-06-14 17:38:56 +0800

    feat(zx): zx学习完毕

--- packages/zx/01.hello.mjs
+++ packages/zx/01.hello.mjs
@@ -1,5 +1,38 @@
-import { $ } from 'zx'
+import { $, echo, os, usePowerShell } from 'zx'
 
-const date = await $`date`
-const res = await $`echo Current date is ${date}.`
-console.log(date, res)
+usePowerShell();
+
+/*const echo = $`echo "hello zx"`
+echo.stdin.write('hello nodejs')
+echo.stdin.end()
+console.log(await echo)*/
+
+/*
+const p = await $`ls -a`
+console.log(p.stdout)
+
+// 等价于
+
+const p = $`ls -a`
+p.stdout.on('data', (buf) => {
+	console.log(buf.toString())
+})*/
+
+/*try {
+	await $`cat notExist`
+} catch (err) {
+	console.log(err)
+	// cat: notExist: No such file or directory
+	// ProcessOutput {
+	//   stdout: '',
+	//   stderr: 'cat: notExist: No such file or directory\n',
+	//   signal: null,
+	//   exitCode: 1
+	// }
+}*/
+
+/*const files = await glob('./!**!/!*.json')
+console.log(files) // [ 'package.json', 'node_modules/zx/package.json' ]*/
+
+process.env.FORCE_COLOR='1'
+await $`cat package.json`
