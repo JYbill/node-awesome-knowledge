5f478efedb5ea2ead24507ec44423431ae943bed
xiaoqinvar <17683860320m0@sina.cn>
2024-11-01 09:38:30 +0800

    feat: 学习class-transform

--- packages/class-transfrom-validator/test1.ts
+++ packages/class-transfrom-validator/test1.ts
@@ -7,21 +7,28 @@ import {IsBoolean, IsNumber, IsString} from 'class-validator';
  * @Author: 小钦var
  * @Date: 2024/11/1 09:22
  */
+
+function logType(target: any, key: string) {
+}
+
 class Test {
 
   // @IsBoolean()
   // age: boolean;
 
-  @Transform((e) => {
-    console.log('transform', e);
-    return e.obj[e.key]; // 结果以最后一次为准
-  })
-  @Expose({ name: 'aka' })
+  // @Transform((e) => {
+  //   console.log('transform', e);
+  //   return e.obj[e.key]; // 结果以最后一次为准
+  // })
+  // @Expose({ name: 'aka' })
+  // name: number;
+
+  @logType
   name: number;
 }
-// const res = plainToClass(Test, {age: 0, name: "false"}, { enableImplicitConversion: true });
-// console.log(res)
+const res = plainToClass(Test, {name: "500"}, { enableImplicitConversion: true });
+console.log(res)
 
-console.log(plainToClass(Boolean, "100"));
+// console.log(plainToClass(Boolean, "100"));
 
 export {}
